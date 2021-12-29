8476356882834f4092d8a74dcf31fea9c3f25f9f
Tong Tai <meotai1999@gmail.com>
2021-12-29 15:57:48 +0700

    xong phim

--- slider.js
+++ slider.js
@@ -1,54 +1,28 @@
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
+var slideIndex = 1;
+showSlides(slideIndex);
 
-const imgss = $$('.slider-item-img img')
-const left = $(".slider-icon__left")
-const right = $(".slider-icon__right")
-const listImg = $(".slider-list-img")
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-currentIndex = 0
-imgs.forEach((img, index) => {
-    currentIndex = index
-})  
-
-right.addEventListener('click', (e) => {
-    currentIndex++
-})
-
-const imgs = [
-    {   
-        id:1,
-        src:'./img/1610356004043_org.jpg'
-    },
-    {   
-        id:2,
-        src:'./img/1615460135754_org.jpg'
-    },
-    {
-        id:3,
-        src:'./img/1631500414107_org.jpg'
-    },
-    {
-        id:4,
-        src:'./img/1634801803471_org.jpg'
-    },
-    {
-        id:5,
-        src:'./img/1636095251577_org.jpg'
-    },
-]
-
-let html = ''
-
-imgs.map((img, index) => {
-    html += `
-        <div class="slider-item-img ">
-            <img src="${img.src}" alt="">
-        </div>
-    `
-})
-
-console.log(html);
-
-listImg.innerHTML = html
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
